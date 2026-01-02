686d4cb312737ca1203866c66389916fddb3fb3f
unknown <marcoslimado12384@gmail.com>
2026-01-02 11:09:28 -0300

    feat(service): metodos de para criar, deletar e alterar a loja com validação de email existente

--- backend/src/service/store.service.js
+++ backend/src/service/store.service.js
@@ -4,11 +4,22 @@ export class StoreService{
     _repository = new StoreRepository();
 
     async register(store) {
-        const existing = await this._repository.findByEmail(store.email);
+        const { name, email, password } = store;
+        const existing = await this._repository.findByEmail(email);
         if (existing.length > 0) {
             throw new Error('usuário já existente');
         }
 
-        return this._repository.create(store);
+        return this._repository.create(name, email, password);
+    }
+
+    async remove(store) {
+        const { idStore } = store;
+        return this._repository.delete(idStore);
+    }
+
+    async update(store) {
+        const { idStore } = store;
+        return this._repository.update(idStore, store);
     }
 }
