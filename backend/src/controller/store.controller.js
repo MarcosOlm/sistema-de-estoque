6abf5078dc668d6f0ca231a2c6f4f7f26cb3eb82
unknown <marcoslimado12384@gmail.com>
2026-01-05 09:43:28 -0300

    fix(controller): retirado o metodo de cadastro da loja

--- backend/src/controller/store.controller.js
+++ backend/src/controller/store.controller.js
@@ -3,24 +3,10 @@ import { StoreService } from "../service/store.service.js";
 export class StoreController {
   _service = new StoreService();
 
-  async create(req, res) {
-    try {
-      const result = await this._service.register(req.body);
-      return res.status(201).json({
-        message: "loja criada com sucesso",
-        result: result,
-      });
-    } catch (err) {
-      return res.status(400).json({
-        error: err.message,
-      });
-    }
-  }
-
   async delete(req, res) {
     try {
       const result = await this._service.remove(req.body);
-      return res.status(201).json({
+      return res.status(200).json({
         message: "loja deletada com sucesso",
         return: result,
       });
@@ -34,7 +20,7 @@ export class StoreController {
   async update(req, res) {
     try {
       const result = await this._service.update(req.body);
-      return res.status(201).json({
+      return res.status(200).json({
         message: "loja alterada com sucesso",
         return: result,
       });
