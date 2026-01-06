614a810b7a4fc0d4083fc102f853434a4d58115b
unknown <marcoslimado12384@gmail.com>
2026-01-06 08:36:08 -0300

    feat(route): adicionado os metodos get, post, delete e put para rota dos produtos

--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -4,6 +4,10 @@ import { ProductController } from '../controller/product.controller.js'
 const ProductRoute = Router();
 const controller = new ProductController();
 
+ProductRoute.get('/', controller.getAll.bind(controller));
+ProductRoute.get('/:idProduct', controller.getById.bind(controller));
 ProductRoute.post('/', controller.create.bind(controller));
+ProductRoute.delete('/', controller.delete.bind(controller));
+ProductRoute.put('/', controller.update.bind(controller));
 
 export default ProductRoute;
