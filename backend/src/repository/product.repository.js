6be86cc96b13f10ad3af227c104432eb8408d12b
unknown <marcoslimado12384@gmail.com>
2026-01-07 08:29:11 -0300

    feat(repository): adicionado o metodo para trazer as informações para o dashboard

--- backend/src/repository/product.repository.js
+++ backend/src/repository/product.repository.js
@@ -3,7 +3,7 @@ import connect from "../database.js";
 export class ProductRepository {
     async getAll(idStore) {
         const db = await connect();
-        const [result] = await db.query("SELECT * FROM products WHERE FK_idStore = ?", [
+        const [result] = await db.query("SELECT name, price, quantity, category FROM products WHERE FK_idStore = ?", [
             idStore,
         ])
         return result;
@@ -17,10 +17,28 @@ export class ProductRepository {
         return result;
     }
 
-    async create(name, price, quantity, idStore) {
+    async getProductsInfo(idStore) {
         const db = await connect();
-        const [result] = await db.query("INSERT INTO products (name, price, quantity, FK_idStore) VALUES (?, ?, ?, ?)", [
-            name, price, quantity, idStore,
+        const [cardQuery] = await db.query("SELECT COUNT(*) AS quantityProducts, ROUND(AVG(price), 2) AS averagePrice, SUM(price) AS amountPrice FROM products WHERE FK_idStore = ?", [
+            idStore,
+        ]);
+        const [graphQuery] = await db.query("SELECT COUNT(*) AS sizeByCategory, ROUND(SUM(price * quantity), 2) AS amountCategory, category FROM products WHERE FK_idStore = ? GROUP BY category", [
+            idStore,
+        ]);
+        const [noStockAlertQuery] = await db.query("SELECT name, quantity, category FROM products WHERE FK_idStore = ? AND quantity < 10", [
+            idStore,
+        ]);
+        const [newProductsQuery] = await db.query("SELECT name, price, category FROM products WHERE FK_idStore = ? ORDER BY idProduct DESC LIMIT 2", [
+            idStore,
+        ]);
+        cardQuery[0].quantityCategory = graphQuery.length;
+        return [{cardQuery: cardQuery[0]}, {graphQuery: [...graphQuery]}, {noStockAlertQuery: [...noStockAlertQuery]}, {newProductsQuery: [...newProductsQuery]}];
+    }
+
+    async create(name, price, quantity, category, idStore) {
+        const db = await connect();
+        const [result] = await db.query("INSERT INTO products (name, price, quantity, category, FK_idStore) VALUES (?, ?, ?, ?, ?)", [
+            name, price, quantity, category, idStore,
         ]);
         return result;
     }
