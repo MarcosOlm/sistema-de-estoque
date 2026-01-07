537f29ab9a43e142ec23522a0563d57de99db26c
unknown <marcoslimado12384@gmail.com>
2026-01-07 08:29:55 -0300

    feat(service): adicionado o metodo para trazer as informações para o dashboard

--- backend/src/service/product.service.js
+++ backend/src/service/product.service.js
@@ -5,7 +5,7 @@ export class ProductService {
 
   async getAllProducts(idStore) {
     const prods = await this._repository.getAll(idStore);
-    if (!prods) {
+    if (!prods || prods.length === 0) {
       throw new Error("não existem produtos");
     }
     return prods;
@@ -13,24 +13,33 @@ export class ProductService {
 
   async getById(idStore, idProduct) {
     const prod = await this._repository.getById(idProduct);
-    if (!prod) {
+    if (!prod || prod.length === 0) {
       throw new Error("não existe o produto informado");
     }
     if (prod[0].FK_idStore != idStore) {
       throw new Error("acesso negado");
     }
+    delete prod[0].FK_idStore;
     return prod[0];
   }
 
+  async getProductsInfo(idStore) {
+    const info = this._repository.getProductsInfo(idStore);
+    if (!info || info.length === 0) {
+      throw new Error('não existem produtos');
+    }
+    return info;
+  }
+
   async register(idStore, product) {
-    const { name, price, quantity } = product;
-    return await this._repository.create(name, price, quantity, idStore);
+    const { name, price, quantity, category } = product;
+    return await this._repository.create(name, price, quantity, category, idStore);
   }
 
   async remove(idStore, product) {
     const { idProduct } = product;
     const prod = await this._repository.getById(idProduct);
-    if (!prod) {
+    if (!prod || prod.length === 0) {
       throw new Error("produto não encontrado");
     }
     if (prod[0].FK_idStore != idStore) {
@@ -42,7 +51,7 @@ export class ProductService {
   async update(idStore, product) {
     const { idProduct } = product;
     const prod = await this._repository.getById(idProduct);
-    if (!prod) {
+    if (!prod || prod.length === 0) {
       throw new Error("produto não encontrado");
     }
     if (prod[0].FK_idStore != idStore) {
