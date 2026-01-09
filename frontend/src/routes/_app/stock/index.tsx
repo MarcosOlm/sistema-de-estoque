22fab932125e959884ada17ed8058c3ea7c14a82
unknown <marcoslimado12384@gmail.com>
2026-01-09 10:34:45 -0300

    feat(page): requisatados os dados do stock e projetando no html com a função de filtro

--- frontend/src/routes/_app/stock/index.tsx
+++ frontend/src/routes/_app/stock/index.tsx
@@ -1,15 +1,78 @@
-import { useState } from "react";
 import "./stock.css";
-import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { useQuery } from "@tanstack/react-query";
+import { useAllProduct } from "../../../hooks/useStock";
 
 export const Route = createFileRoute("/_app/stock/")({
+  validateSearch: (search: Record<string, unknown>) => {
+    return {
+      query: search.query as string,
+      category: search.category as string,
+      price: search.price,
+      quantity: search.quantity,
+    };
+  },
   component: RouteComponent,
 });
 
 // Parametro opcional. Se não tiver parametro, mostrar todos.
 
 function RouteComponent() {
+  const { query, category, price, quantity } = Route.useSearch();
   const [filter, isFilter] = useState<boolean>(false);
+  const navegate = useNavigate({ from: Route.fullPath });
+  const { data, isLoading } = useQuery(useAllProduct());
+  if (isLoading) {
+    return <p>Carregando...</p>;
+  }
+  let lagerPrice = 0;
+  let lagerQuantity = 0;
+  const products = data?.result
+    .filter((prod, index) => {
+      if (index == 1) {
+        lagerPrice = prod.price
+        lagerQuantity = prod.quantity
+      }
+      else {
+        if (prod.price > lagerPrice) {
+          lagerPrice = prod.price
+        }
+        if (prod.quantity > lagerQuantity) {
+          lagerQuantity = prod.quantity
+        }
+      }
+      if (query && prod.name.startsWith(query)) {
+        return prod;
+      }
+      if (!query) {
+        return prod;
+      }
+    })
+    .filter((prod) => {
+      if (category && prod.category === category) {
+        return prod;
+      }
+      if (!category) {
+        return prod;
+      }
+    })
+    .filter((prod) => {
+      if (price && prod.price <= Number(price)) {
+        return prod;
+      }
+      if (!price) {
+        return prod;
+      }
+    })
+    .filter((prod) => {
+      if (quantity && prod.quantity <= Number(quantity)) {
+        return prod;
+      }
+      if (!quantity) {
+        return prod;
+      }
+    });
 
   return (
     <>
@@ -20,7 +83,15 @@ function RouteComponent() {
       </section>
       <section className="search-filter">
         <div className="search">
-          <input type="text" placeholder="🔍 Digite o nome do produto..." />
+          <input
+            type="text"
+            placeholder="🔍 Digite o nome do produto..."
+            onChange={(e) =>
+              navegate({
+                search: (prev) => ({ ...prev, query: e.target.value }),
+              })
+            }
+          />
           <button type="button" onClick={() => isFilter(!filter)}>
             Filtro
           </button>
@@ -30,37 +101,66 @@ function RouteComponent() {
             <h1>Filtros Avançados</h1>
             <div className="category-wrap">
               <label htmlFor="category">Categoria</label>
-              <select id="category">
-                <option value="">Eletrônico</option>
-                <option value="">Imóveis</option>
-                <option value="">Perecíveis</option>
+              <select
+                id="category"
+                onChange={(e) => {
+                  navegate({
+                    search: (prev) => ({ ...prev, category: e.target.value }),
+                  });
+                }}
+              >
+                <option value="eletrônico">Eletrônico</option>
+                <option value="vestuário">Vestuário</option>
+                <option value="perecível">Perecíveis</option>
               </select>
             </div>
             <div className="price-wrap">
-              <label htmlFor="price">Preço: R$ 0 - R$ 1000</label>
-              <input type="range" id="price" />
+              <label htmlFor="price">Preço: R$ 0 - R$ {lagerPrice} </label>
+              <input
+                type="range"
+                id="price"
+                min={0}
+                max={lagerPrice}
+                onChange={(e) =>
+                  navegate({
+                    search: (prev) => ({ ...prev, price: e.target.value }),
+                  })
+                }
+              />
             </div>
             <div className="quant-wrap">
-              <label htmlFor="quant">Quantidade: 0 - 100</label>
-              <input type="range" id="quant" />
+              <label htmlFor="quant">Quantidade: 0 - {lagerQuantity} </label>
+              <input
+                type="range"
+                id="quant"
+                min={0}
+                max={lagerQuantity}
+                onChange={(e) =>
+                  navegate({
+                    search: (prev) => ({ ...prev, quantity: e.target.value }),
+                  })
+                }
+              />
             </div>
           </div>
         )}
       </section>
       <div className="prods-title">
-        <h1>2 produtos encontrados</h1>
+        <h1> {products?.length} produtos encontrados</h1>
       </div>
       <section className="prods-wrap">
-        <article className="product">
-          <div className="prod-name">
-            <h1>Celular</h1>
-            <p>Eletrônico</p>
-          </div>
-          <div className="prod-price">
-            <h2>R$ 1.000,00</h2>
-            <p>2 em estoque</p>
-          </div>
-        </article>
+        {products?.map((item) => (
+          <article className="product" key={item.idProduct}>
+            <div className="prod-name">
+              <h1> {item.name} </h1>
+              <p> {item.category} </p>
+            </div>
+            <div className="prod-price">
+              <h2>R$ {item.price} </h2>
+              <p> {item.quantity} em estoque</p>
+            </div>
+          </article>
+        ))}
       </section>
     </>
   );
