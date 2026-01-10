990e15a5b30cfb715d1d92b96b74e0a46bd0c78a
unknown <marcoslimado12384@gmail.com>
2026-01-10 10:49:16 -0300

    feat(service): adicionado os metodos para criar e alterar o produto no sistema

--- frontend/src/services/product.service.ts
+++ frontend/src/services/product.service.ts
@@ -1,10 +1,22 @@
 import { api } from "../api/api";
-import type { allProductResponse, product } from "../types/product";
+import type { allProductResponse, defaultResponse, product, productResponse } from "../types/product";
 
 export const getAllProduct = async (): Promise<allProductResponse> => {
     return (await api.get('/product')).data;
 }
 
-export const getProductById = async (id: number): Promise<product> => {
+export const getProductById = async (id: number): Promise<productResponse> => {
     return (await api.get(`/product/${id}`)).data;
+}
+
+export const createProduct = async (data: product): Promise<productResponse> => {
+    return (await api.post('/product', data)).data;
+}
+
+export const updateProduct = async (data: product): Promise<defaultResponse> => {
+    return (await api.put('/product', data)).data;
+}
+
+export const deleteProduct = async (): Promise<defaultResponse> => {
+    return (await api.delete('/product')).data;
 }
