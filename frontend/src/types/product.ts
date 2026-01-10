af6f15d0bbce3442631d20687d3dad93739ff9f3
unknown <marcoslimado12384@gmail.com>
2026-01-10 10:48:27 -0300

    feat(type): adicionado o tipo do produto

--- frontend/src/types/product.ts
+++ frontend/src/types/product.ts
@@ -1,12 +1,21 @@
 export interface product {
-    idProduct: number,
-    name: string,
-    price: number,
-    quantity: number,
-    category: string,
+  idProduct?: number;
+  name: string;
+  price: number;
+  quantity: number;
+  category: string;
 }
 
 export interface allProductResponse {
-    message: string,
-    result: product[],
+  message: string;
+  result: product[];
+}
+
+export interface productResponse {
+  message: string;
+  result: product;
+}
+
+export interface defaultResponse {
+  message: string;
 }
