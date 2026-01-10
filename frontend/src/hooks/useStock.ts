6fc8083d416e0eb4e9fd18815a1ab663915c0c19
unknown <marcoslimado12384@gmail.com>
2026-01-10 10:51:14 -0300

    feat(hook): adicionado os hooks do tanstack query para enviar os dados e gerenciar o cache

--- frontend/src/hooks/useStock.ts
+++ frontend/src/hooks/useStock.ts
@@ -1,16 +1,44 @@
-import { queryOptions } from "@tanstack/react-query";
-import { getAllProduct, getProductById } from "../services/product.service";
+import { queryOptions, useMutation } from "@tanstack/react-query";
+import {
+  createProduct,
+  getAllProduct,
+  getProductById,
+  updateProduct,
+} from "../services/product.service";
+import type { product } from "../types/product";
+import { useNavigate } from "@tanstack/react-router";
 
 export function useAllProduct() {
-    return queryOptions({
-        queryKey: ['products'],
-        queryFn: getAllProduct,
-    })
+  return queryOptions({
+    queryKey: ["products"],
+    queryFn: getAllProduct,
+    retry: 1,
+  });
 }
 
 export function useProductById(id: number) {
-    return queryOptions({
-        queryKey: ['product', id],
-        queryFn: () => getProductById(id),
-    })
+  return queryOptions({
+    queryKey: ["product", id],
+    queryFn: () => getProductById(id),
+  });
+}
+
+export function useCreateProduct() {
+  const navigate = useNavigate();
+  return useMutation({
+    mutationFn: (data: product) => createProduct(data),
+    onSuccess: () => {
+      navigate({ to: "/" });
+    },
+  });
+}
+
+export function useUpdateProduct() {
+  const navigate = useNavigate();
+  return useMutation({
+    mutationFn: (data: product) => updateProduct(data),
+    onSuccess: () => {
+      navigate({ to: "/dashboard" });
+    },
+  });
 }
